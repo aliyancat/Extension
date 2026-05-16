3275b91cfd9515f8ca8ed5e82199907683f5e799
aliyancat <aliyanmunawwaralis@gmail.com>
2026-05-16 16:01:24 +0500

    --- Auto Git Commit ---

--- background.js
+++ background.js
@@ -60,7 +60,12 @@ async function handlePDF(url) {
         canvas.width = viewport.width;
         canvas.height = viewport.height;
         await page.render({ canvasContext: canvas.getContext('2d'), viewport }).promise;
-        const { data: { text } } = await Tesseract.recognize(canvas, 'eng');
+        const { data: { text } } = await Tesseract.recognize(canvas, 'eng', {
+        scale: 3,
+        grayscale: true,
+        contrast: 1.5,
+        tessedit_pageseg_mode: 1,
+      });
         fullText += text + '\n';
       }
 
